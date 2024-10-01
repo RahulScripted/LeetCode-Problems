509b44602e9886e004261da1065f1f8fc12abe12
CodingWizard2001 <goswami.rahul1002@gmail.com>
2024-10-02 00:19:14 +0530

    Today's problem solved

---
+++ Top Interview 150/Array Or Strings/_169_MajorityElement.cpp
@@ -0,0 +1,52 @@
+// Given an array nums of size n, return the majority element. The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        // Moore algorithm
+        int count = 0;
+        int candidate = 0;
+        
+        for (int num : nums) {
+            if (count == 0) candidate = num;
+            if (num == candidate) count++;
+            else count--;
+        }
+        
+        return candidate;
+
+        // Using map
+        // int n = nums.size();
+        // unordered_map<int,int>st;
+        // for(int i = 0;i < n;i++) st[nums[i]]++;
+
+        // n /= 2;
+        // for(auto x : st){
+        //     if(x.second > n) return x.first;
+        // }
+        // return -1;
+
+        // Sorting
+        // int n = nums.size();
+        // sort(nums.begin(),nums.end());
+        // return nums[n / 2];
+    }
+};
+
+int main(){
+    Solution solve;
+
+    // 1st example
+    vector<int>nums = {2,2,1,1,1,2,2};
+    cout<<"Majority element : "<<solve.majorityElement(nums)<<endl;
+
+    // 2nd example
+    nums = {3,2,3};
+    cout<<"Majority element : "<<solve.majorityElement(nums)<<endl;
+}
